4b9ecd33a20a1bd4afe6c386c08084c0b445e4cd
Max Schanner <arthus@gfz-potsdam.de>
2024-02-29 12:01:22 +0100

    Reformatting.

--- python/pyhamstr/stan_hamstr.py
+++ python/pyhamstr/stan_hamstr.py
@@ -8,24 +8,37 @@ import datetime
 
 from make_stan_dat_hamstr import make_stan_dat_hamstr, get_inits_hamstr
 
-def hamstr(depth, obs_age, obs_err,
-           min_age=None, K_fine=None, K_factor=None,
-           top_depth=None, bottom_depth=None,
-           acc_mean_prior=None, acc_shape=1.5,
-           mem_mean=0.5, mem_strength=10,
-           model_bioturbation=False,
-           n_ind=None,
-           L_prior_mean=10, L_prior_shape=2, L_prior_sigma=None,
-           model_displacement=False,
-           D_prior_scale=10,
-           model_hiatus=False,
-           H_top=None, H_bottom=None,
-           sample_posterior=True,
-           hamstr_control={},
-           stan_sampler_args={}):
+
+def hamstr(
+    depth,
+    obs_age,
+    obs_err,
+    min_age=None,
+    K_fine=None,
+    K_factor=None,
+    top_depth=None,
+    bottom_depth=None,
+    acc_mean_prior=None,
+    acc_shape=1.5,
+    mem_mean=0.5,
+    mem_strength=10,
+    model_bioturbation=False,
+    n_ind=None,
+    L_prior_mean=10,
+    L_prior_shape=2,
+    L_prior_sigma=None,
+    model_displacement=False,
+    D_prior_scale=10,
+    model_hiatus=False,
+    H_top=None,
+    H_bottom=None,
+    sample_posterior=True,
+    hamstr_control={},
+    stan_sampler_args={}
+):
     if min_age is None:
         min_age = 1950 - datetime.datetime.now().year
-    
+
     if K_fine is not None:
         if K_fine < 2:
             raise ValueError("A minimum of 2 sections are required")
@@ -66,14 +79,23 @@ def hamstr(depth, obs_age, obs_err,
     used_sampler_args = get_stan_sampler_args(**stan_sampler_args)
     np.random.seed(used_sampler_args['seed'])
 
-    inits = [get_inits_hamstr(stan_dat) for _ in range(used_sampler_args['chains'])]
+    inits = [
+        get_inits_hamstr(stan_dat) for _ in range(used_sampler_args['chains'])
+    ]
 
     model = CmdStanModel(stan_file="../inst/stan/hamstr.stan")
     fit = None
     if sample_posterior:
-        fit = model.sample(data=stan_dat, inits=inits, **used_sampler_args, show_console=True)
+        fit = model.sample(
+            data=stan_dat,
+            inits=inits,
+            show_console=True,
+            **used_sampler_args,
+        )
     else:
-        print("Skipping posterior sampling as per the 'sample_posterior' flag.")
+        print(
+            "Skipping posterior sampling as per the 'sample_posterior' flag."
+        )
     stan_dat.update(used_sampler_args)
     stan_dat.pop("hamstr_control")
     stan_dat.pop("stan_sampler_args")
@@ -81,54 +103,71 @@ def hamstr(depth, obs_age, obs_err,
     # az.to_netcdf(inference_data, "model_results.nc")
     return inference_data
 
-def get_stan_sampler_args(chains=1,
-                          iter=2000,
-                          warmup=None,
-                          thin=1,
-                          seed=None,
-                          check_data=True,
-                          sample_file=None,
-                          diagnostic_file=None,
-                          verbose=False,
-                          algorithm=("NUTS", "HMC", "Fixed_param"),
-                          control=None,
-                          include=True,
-                          open_progress=None,
-                          show_messages=True,
-                          **kwargs):
+
+def get_stan_sampler_args(
+    chains=1,
+    iter=2000,
+    warmup=None,
+    thin=1,
+    seed=None,
+    check_data=True,
+    sample_file=None,
+    diagnostic_file=None,
+    verbose=False,
+    algorithm=("NUTS", "HMC", "Fixed_param"),
+    control=None,
+    include=True,
+    open_progress=None,
+    show_messages=True,
+    **kwargs,
+):
     if warmup is None:
         warmup = iter // 2
     if seed is None:
         seed = np.random.randint(1, np.iinfo(np.int32).max)
     if open_progress is None:
-        open_progress = sys.stdout.isatty() and os.environ.get("RSTUDIO") != "1"
-    
+        open_progress = (
+            sys.stdout.isatty() and os.environ.get("RSTUDIO") != "1"
+        )
+
     # Additional arguments passed via kwargs are added to the dictionary
     sampler_args = {
         "chains": chains,
         # "cores": chains, Not available
-        # "iter": iter, Does not exists its split into iter_warmup and iter_sampling
+        # "iter": iter, Does not exists its split into
+        # iter_warmup and iter_sampling
         "iter_warmup": warmup,
         "iter_sampling": iter - warmup,
         "thin": thin,
         "seed": seed,
-        # "check_data": check_data, not neede since CmdStanPy performs necessary data checks implicitly
+        # "check_data": check_data, not needed since CmdStanPy performs
+        # necessary data checks implicitly
         # "sample_file": sample_file,
         "output_dir": sample_file,
-        # CmdStanPy provides diagnostic information through its interfaces and the resulting CmdStanMCMC object from the sample() method.
-        # While it doesn't directly accept a diagnostic_file argument, you can access diagnostic statistics and perform checks programmatically.
-        # Additionally, CmdStanPy saves all sampling output, including diagnostics, to CSV files by default.
-        # You can specify the directory where these files are saved using the output_dir argument, and then process these files as needed for diagnostics.
-        # For example, to access summary diagnostics from the fit object: print(fit.summary())
+        # CmdStanPy provides diagnostic information through its interfaces and
+        # the resulting CmdStanMCMC object from the sample() method.
+        # While it doesn't directly accept a diagnostic_file argument, you can
+        # access diagnostic statistics and perform checks programmatically.
+        # Additionally, CmdStanPy saves all sampling output, including
+        # diagnostics, to CSV files by default.
+        # You can specify the directory where these files are saved using the
+        # output_dir argument, and then process these files as needed for
+        # diagnostics.
+        # For example, to access summary diagnostics from the fit object:
+        # print(fit.summary())
         # "diagnostic_file": diagnostic_file,
-        # CmdStanPy does not accept verbose as a direct argument, which contrasts with some other interfaces for Stan
-        # where verbose might control the amount of output printed to the console during sampling.
-        # In CmdStanPy, verbosity can be controlled through the Python logging module
+        # CmdStanPy does not accept verbose as a direct argument, which
+        # contrasts with some other interfaces for Stan where verbose might
+        # control the amount of output printed to the console during sampling.
+        # In CmdStanPy, verbosity can be controlled through the Python logging
+        # module
         # import logging
         # logging.basicConfig(level=logging.DEBUG)
         # "verbose": verbose,
-        # CmdStanPy does not provide a direct Python argument to switch between algorithms like "NUTS", "HMC", or "Fixed_param".
-        # The default and generally recommended algorithm for continuous parameters models is NUTS, which is automatically used by CmdStanPy.
+        # CmdStanPy does not provide a direct Python argument to switch between
+        # algorithms like "NUTS", "HMC", or "Fixed_param".
+        # The default and generally recommended algorithm for continuous
+        # parameters models is NUTS, which is automatically used by CmdStanPy.
         # "algorithm": algorithm,
         # "control": control,
         # "include": include, Not available
@@ -136,5 +175,5 @@ def get_stan_sampler_args(chains=1,
         # "show_messages": show_messages, Not available
     }
     sampler_args.update(kwargs)
-    
+
     return sampler_args
