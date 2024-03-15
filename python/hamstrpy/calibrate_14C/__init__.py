78abb9603cf197d8110d917d70519942e4e6b34d
Max Schanner <arthus@gfz-potsdam.de>
2024-03-15 15:05:53 +0100

    Move warnings and remove calibrate_rc_data.

--- python/hamstrpy/calibrate_14C/__init__.py
+++ python/hamstrpy/calibrate_14C/__init__.py
@@ -1,12 +1,19 @@
+import warnings
+
 from .calibration_curves import intcal20, shcal20, marine20
-from .calibrate import calibrate_14C_age, calibrate_rc_data
+from .calibrate import calibrate_14C_age
+
+# Monkey-patch the line away from warnings, as it is rather irritating.
+formatwarning_orig = warnings.formatwarning
+warnings.formatwarning = lambda msg, cat, fname, lineno, line=None: \
+    formatwarning_orig(msg, cat, fname, lineno, line='')
+
 
 __all__ = [
     'intcal20',
     'shcal20',
     'marine20',
     'calibrate_14C_age',
-    'calibrate_rc_data',
 ]
 
 packagename = "hamstrpy"
