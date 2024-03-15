0494afc8b8c4f1f5071805ebfcd702d47e615ae2
Max Schanner <arthus@gfz-potsdam.de>
2024-03-15 13:40:09 +0100

    Add modules.

--- python/hamstrpy/__init__.py
+++ python/hamstrpy/__init__.py
@@ -1,1 +1,13 @@
+from . import calibrate_14C
+from .make_stan_dat_hamstr import make_stan_dat_hamstr, get_inits_hamstr
+from .stan_hamstr import hamstr, get_stan_sampler_args
+
+__all__ = [
+    'calibrate_14C',
+    'make_stan_dat_hamstr',
+    'get_inits_hamstr',
+    'hamstr',
+    'get_stan_sampler_args',
+]
+
 __version__ = '0.0.1'
