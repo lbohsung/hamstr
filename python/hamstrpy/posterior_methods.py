468a29d57899bcfcf2773d5f59ece85b6ff5d406
Max Schanner <arthus@gfz-potsdam.de>
2024-03-18 13:19:56 +0100

    Modify get_posterior_ages to return arrays; add check_rhat.

--- python/hamstrpy/posterior_methods.py
+++ python/hamstrpy/posterior_methods.py
@@ -1,22 +1,34 @@
-import numpy as np
-import pandas as pd
+from warnings import warn
+
+import arviz as az
 
 
-def get_posterior_ages(idata):
+def get_posterior_ages(idata, thin=1):
     modelled_depths = idata.observed_data['modelled_depths'].values
     posterior_ages = idata.posterior['c_ages'].values
 
-    n_iters, n_depths = posterior_ages.shape[1], posterior_ages.shape[2]
-    iter_idx, depth_idx = np.meshgrid(
-        np.arange(n_iters),
-        np.arange(n_depths),
-        indexing='ij',
-    )
-    df = pd.DataFrame({
-        'iter': iter_idx.ravel() + 1,
-        'depth': modelled_depths[depth_idx.ravel()],
-        'age': posterior_ages[0, iter_idx.ravel(), depth_idx.ravel()],
-    })
-    df.sort_values(by=['iter', 'depth'], inplace=True)
+    posterior_ages = posterior_ages.reshape(-1, modelled_depths.shape[0])
+    posterior_ages = posterior_ages[::thin].T
 
-    return df
+    return modelled_depths, posterior_ages
+
+
+def check_rhat(idata_or_summary, rhat, return_failed=False):
+    if isinstance(idata_or_summary, az.InferenceData):
+        summary = az.summary(idata_or_summary)
+    else:
+        summary = idata_or_summary
+
+    rhat_failed = summary.query(f"{rhat} < r_hat ")
+
+    if 0 < len(rhat_failed):
+        warn(
+            'Not all random variables pass the R hat criterion',
+            UserWarning,
+        )
+        print(rhat_failed)
+        if return_failed:
+            return False, rhat_failed
+        return False
+    else:
+        return True
