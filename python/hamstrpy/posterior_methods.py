e30c1666d7ea747aab5a29e8e3f03f00b6dc7ae1
Arthus <schannermax@googlemail.com>
2024-03-21 10:14:21 +0100

    Add docstrings.

--- python/hamstrpy/posterior_methods.py
+++ python/hamstrpy/posterior_methods.py
@@ -4,6 +4,22 @@ import arviz as az
 
 
 def get_posterior_ages(idata, thin=1):
+    """ Extract the posterior ages from sampling result
+
+    Parameters
+    ----------
+    idata : arviz.InferenceData
+        The sampling result returned by the hamstr function
+    thin : int, optional
+        Thinning factor. This will slice the samples by ::thin
+
+    Returns
+    -------
+    array of shape n
+        The modelled dephts, useful for plotting etc.
+    array of shape n x n_samples // thin
+        The posterior age samples
+    """
     modelled_depths = idata.observed_data['modelled_depths'].values
     posterior_ages = idata.posterior['c_ages'].values
 
@@ -14,6 +30,26 @@ def get_posterior_ages(idata, thin=1):
 
 
 def check_rhat(idata_or_summary, rhat, return_failed=False):
+    """ Check if an rhat citerion is passed by all random variables
+
+    Parameters
+    ----------
+    idata_or_summary : arviz.InferenceData or pandas.DataFrame
+        Either a result returned by the hamstr function or the return value
+        from a call to arviz.summary.
+    rhat : float
+        The rhat criterion to be checked.
+    return_failed : bool, optional
+        Whether to return the part of the summary that fails the criterion.
+
+    Returns
+    -------
+    bool
+        Whether all random variables pass the criterion.
+    DataFrame
+        A dataframe of failing random variables. Only returned if return_failed
+        is True (default is False)
+    """
     if isinstance(idata_or_summary, az.InferenceData):
         summary = az.summary(idata_or_summary)
     else:
