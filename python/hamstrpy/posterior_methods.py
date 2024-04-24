8aeedb91962bafd12ddafc879d1e2dd24f8237f7
Max Schanner <arthus@gfz-potsdam.de>
2024-04-24 13:37:28 +0200

    Update docstrings to respect linewidth.

--- python/hamstrpy/posterior_methods.py
+++ python/hamstrpy/posterior_methods.py
@@ -1,5 +1,5 @@
 from warnings import warn
-
+from scipy.interpolate import interp1d
 import arviz as az
 
 
@@ -27,6 +27,99 @@ def get_posterior_ages(idata, thin=1):
     posterior_ages = posterior_ages[::thin].T
 
     return modelled_depths, posterior_ages
+
+
+def get_interpolated_mean_and_std(idata):
+    """
+    Calculate the depth-to-time conversion functions based on posterior age
+    distributions.
+
+    This function computes the mean and standard deviation of the age
+    distributions for modelled depths, and returns functions that interpolate
+    these values across depths. The ages are adjusted to calendar years by
+    subtracting them from 1950.
+
+    Parameters
+    ----------
+    idata : arviz.InferenceData
+        The sampling result returned by the hamstr function
+
+    Returns
+    -------
+    d2t_mean : scipy.interpolate.interpolate.interp1d
+        An interpolation function for the mean posterior age (in calendar
+        years) as a function of modelled depth. This function can be called
+        with an array of depths to obtain ages for depths of a given sediment
+        record.
+    d2t_std : scipy.interpolate.interpolate.interp1d
+        An interpolation function for the standard deviation of the posterior
+        ages (in calendar years) as a function of modelled depth. This function
+        can be called with an array of depths to obtain ages uncertainties for
+        depths of a given sediment record.
+
+    Notes
+    -----
+    The interpolation kind is set to 'linear', and extrapolation is allowed for
+    depths outside the range of the modelled depths. This behavior can be
+    customized by modifying the 'kind' and 'fill_value' parameters of the
+    interp1d functions within the code.
+   """
+    post_depths, post_ages = get_posterior_ages(idata)
+    post_ages = 1950 - post_ages
+    t_mean = post_ages.mean(axis=1)
+    t_std = post_ages.std(axis=1)
+    d2t_mean = interp1d(
+        post_depths,
+        t_mean,
+        kind='linear',
+        fill_value="extrapolate",
+    )
+    d2t_std = interp1d(
+        post_depths,
+        t_std,
+        kind='linear',
+        fill_value="extrapolate",
+    )
+    return d2t_mean, d2t_std
+
+
+def interpolate_adm(idata):
+    """
+    Calculate the depth-to-time conversion functions based on posterior age
+    distributions.
+
+    This function linearly interpolates every posterior sample.
+    The ages are adjusted to calendar years by subtracting them from 1950.
+
+    Parameters
+    ----------
+    idata : arviz.InferenceData
+        The sampling result returned by the hamstr function
+
+    Returns
+    -------
+    interpolated_adm : scipy.interpolate.interpolate.interp1d
+        An interpolation function for the all posterior samples (in calendar
+        years) as a function of modelled depth. This function can be called
+        with an array of depths to obtain posterior ages for depths of a given
+        sediment record.
+
+    Notes
+    -----
+    The interpolation kind is set to 'linear', and extrapolation is allowed for
+    depths outside the range of the modelled depths. This behavior can be
+    customized by modifying the 'kind' and 'fill_value' parameters of the
+    interp1d functions within the code.
+   """
+    post_depths, post_ages = get_posterior_ages(idata)
+    interpolated_adm = interp1d(
+        post_depths,
+        1950 - post_ages,
+        kind='linear',
+        fill_value="extrapolate",
+        axis=0,
+    )
+    return interpolated_adm
 
 
 def check_rhat(idata_or_summary, rhat, return_failed=False):
